5d989f06d03f6926bff5596d481dfaeffc101f20
Lucas Pinheiro <lucas.pinheiro@spacesheepgames.com>
2022-06-23 15:50:53 -0300

    Items list

---
+++ Items.lua
@@ -0,0 +1,12 @@
+return {
+    "12_years_old_whiskey",
+    "19th_century_swedish_skaktetra",
+    "50s_skirt",
+    "2020_mustang",
+    "Adult_puzzle",
+    "Animals_lunchbox",
+    "Antique_straight_razor",
+    "Best_dad_mug",
+    "Boyfriend_doll",
+    "buggy"
+}
