fdcce9a078702f3ba4806b4c3454379926cba9e7
Lucas Pinheiro <lucas.pinheiro@spacesheepgames.com>
2022-06-23 17:11:24 -0300

    Implement final victory popup

---
+++ legendaryVictory.lua
@@ -0,0 +1,44 @@
+local legendaryVictory = {}
+
+function legendaryVictory.load()
+end
+
+function legendaryVictory.update(dt)
+end
+
+function legendaryVictory.draw()
+    -- background
+    local screenWidth, screenHeight = love.graphics.getDimensions()
+    local rectWidth = screenWidth * 0.85
+    local rectHeight = screenHeight * 0.8
+
+    local color = {
+        fill = {0, 0, 0, 1},
+        line = {1, 1, 1, 1}
+    }
+    for _, mode in ipairs {"fill", "line"} do
+        love.graphics.setColor(color[mode])
+        love.graphics.rectangle(
+            mode,
+            screenWidth / 2 - rectWidth / 2,
+            screenHeight / 2 - rectHeight / 2,
+            rectWidth,
+            rectHeight
+        )
+    end
+
+    love.graphics.draw(
+        legendaryItem,
+        screenWidth / 2,
+        screenHeight / 2,
+        0,
+        1,
+        1,
+        legendaryItem:getWidth() / 2,
+        legendaryItem:getHeight() / 2
+    )
+
+    love.graphics.print("Você ganhou!", 100, screenHeight * 0.8, 0, 2)
+end
+
+return legendaryVictory
